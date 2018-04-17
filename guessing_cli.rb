9bcd6dfc27ee0a26253a9a27b7c075163e6941b0
Sangeetha Rajeev <sang.rajeev@gmail.com>
2018-04-17 21:07:26 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -8,7 +8,7 @@ def run_guessing_game
     
     if guess == "exit"
       puts "Goodbye!"
-      return
+      break
     elsif guess == computer
       puts "You guessed the correct number!"
     else
